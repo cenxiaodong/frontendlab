0c866a2c65e32965b50211d658a50a9b623fd2f7
cxd <cenxiao999@163.com>
2025-11-10 18:33:44 +0800

    chore: 添加pinia

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import router from './router';
+import { createPinia } from 'pinia';
 
 import 'normalize.css';
 
 import '@/assets/styles/_reset.scss';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(createPinia()).mount('#app');
